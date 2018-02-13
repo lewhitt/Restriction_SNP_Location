a9e94f3d77e77e888c83d58d80fe35efc8146682
lewhitt <33207287+lewhitt@users.noreply.github.com>
2018-02-13 11:38:05 -0600

    Update CdRestrictionSiteLocator.py
    Clean up of comments in the script. Currently only good for files containing one sequence. Should update at some point.

--- CdRestrictionSiteLocator.py
+++ CdRestrictionSiteLocator.py
@@ -8,7 +8,6 @@ def main():
     parser.add_option("-r", "--restrSeq", dest="restrictionSeq", help="restriction sequence")
     parser.add_option("-o", "--output", dest="output_file", default="CdRestrictionSiteLocator_output.txt", help="name of output file with the coordinates of all site matches")
     (options, args) = parser.parse_args()
-    ##with open("testfile", "r") as file:
     with open("Zea_mays.AGPv3.22.dna.chromosome.2.cd.mapping.section.fa", "r") as file:
         for line in file.readlines():
             if line[0] == ">":
@@ -16,15 +15,14 @@ def main():
             else:
                 chr2_region = line.strip()
                 ##print chr2_region[780:786]
-                print chr2_region[168990545-159243918:168990551-159243918] 
+                ##print chr2_region[168990545-159243918:168990551-159243918] 
                 pattern = str(options.restrictionSeq)
                 regex = re.compile(pattern)
                 with open("outputHolder", "w") as output:
                     for match in regex.finditer(chr2_region):
                         correct = match.start()+ 159243918
-                        ##span = match.span()
                         output.write(str(correct)+'\n')
-    os.rename("outputHolder", options.output_file+'_regions')
+    os.rename("outputHolder", options.output_file+'_sites')
     sys.exit()
 if __name__ == '__main__':
     main()
