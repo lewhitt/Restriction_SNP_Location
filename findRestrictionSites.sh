af222cd37560aa39a9b911e1099f70b620a5b089
lewhitt <33207287+lewhitt@users.noreply.github.com>
2018-02-13 11:47:35 -0600

    Update findRestrictionSites.sh
    Added a new for loop for finding _sites files in Mac (currently commented out). Default is linux find function using -printf.

--- findRestrictionSites.sh
+++ findRestrictionSites.sh
@@ -1,6 +1,6 @@
 ##This file runs CdRestrictionSiteLocator.py and SNP_RestrictionMatching.py
-##for every restriction enzyme in the list. Make sure the seqs match the
-##name of their corresponding enzyme.
+#for every restriction enzyme in the list. Make sure the seqs match the
+#name of their corresponding enzyme.
 
 enzymes=("bamHI" "ecoRI" "hindIII" "pstl" "pvull" "xhol")
 seqs=("GGATCC" "GAATTC" "AAGCTT" "CTGCAG" "CAGCTG" "CTCGAG")
@@ -12,7 +12,12 @@ do
 done
 
 regions=()
-for f in `find . -type f -name '*_regions' -printf "%f\n"`; 
+##MacOS does not support -printf funciton in find. If using MacOS us the for loop on line 19, but be aware that depending on
+#the directory this is ran in, print $N might have to change depending on which backslash field contains the file name. If
+#ran in directory holding all the scripts, find output will be ./example_site, and thus the second field has the filename.
+
+##for f in `find . -type f -name '*_sites' | awk -F/ '{print $2}'`;
+for f in `find . -type f -name '*_sites' -printf "%f\n"`; 
 do 
     regions+=($f)
 done
@@ -24,4 +29,3 @@ for ((q=0; q<${arrLen}; q++));
 do
     python SNP_RestrictionMatching.py -r ${regions[$q]} -s coordSNP.txt -o ${enzymes[$q]}
 done
-
